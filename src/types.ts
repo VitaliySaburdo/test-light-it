3a4e4cefd1176ba8a54766a1f235338db17eb2bb
VitaliySaburdo <vsaburdo@gmail.com>
2024-01-21 17:18:09 +0200

    types add

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  id: string;
+  title: string;
+  image: string;
+  text: string;
+}
+
+export interface Post {
+    id: string,
+    rate: number,
+    text: string,
+    id_user: string,
+    id_entry: string
+}
+
